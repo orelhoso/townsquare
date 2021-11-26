c2e5d4eb4ec196dd17c6706987e340bd6a00320c
botcbr <74988849+botcbr@users.noreply.github.com>
2021-11-26 08:32:15 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // if the app is supposed to run on Github Pages in a subfolder, use the following config:
+  // if the app is supposed to run on Github Pages in a subfolder, use the following config:1
   publicPath: process.env.NODE_ENV === "production" ? "/townsquare/" : "/"
   //publicPath: process.env.NODE_ENV === "production" ? "/" : "/"
 };
